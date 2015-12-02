27c4bff82cd91c26513e247acff925aa5ca2c5c5
Karl Hughes <khughes.me@gmail.com>
2015-12-02 12:18:26 -0500

    Adding client and tests

---
+++ src/AnswerbaseClient.php
@@ -0,0 +1,113 @@
+<?php namespace Packback\Answerbase;
+
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Psr7\Response;
+
+class AnswerbaseClient
+{
+    public function __construct($config = [])
+    {
+        $this->parameters = [
+            'apikey' => $config['api_key'],
+            'format' => 'json',
+        ];
+        $this->client = new HttpClient([
+            'base_uri' => $config['base_uri'],
+            'timeout'  => $config['timeout'],
+        ]);
+    }
+
+    public function createUser($attributes = [])
+    {
+        $response = $this->post('registeruser.aspx', $attributes);
+
+        if ($response && $response->getStatusCode() == 200) {
+            return $this->getData($response);
+        }
+        return [];
+    }
+
+    public function getCategories($parameters = [])
+    {
+        $defaults = [
+            'maxresults' => '0',
+            'restrictToFirstLevel' => 'true',
+        ];
+
+        $response = $this->get('getcategories.aspx', array_merge($defaults, $parameters));
+
+        if ($response && $response->getStatusCode() == 200) {
+            return $this->getData($response);
+        }
+        return [];
+    }
+
+    public function getQuestionsByCategoryId($category_id = null, $parameters = [])
+    {
+        $defaults = [
+            'categoryIds' => $category_id,
+            'orderby' => 'newest',
+            'fullquestiondetails' => 'true',
+            'maxresults' => '0',
+        ];
+
+        $response = $this->get('getquestionslist.aspx', array_merge($defaults, $parameters));
+
+        if ($response && $response->getStatusCode() == 200) {
+            return $this->getData($response);
+        }
+        return [];
+    }
+
+    public function getUserByEmail($email = '')
+    {
+        $response = $this->get('getuser.aspx', ['useremail' => $email]);
+
+        if ($response && $response->getStatusCode() == 200) {
+            return $this->getData($response);
+        }
+        return [];
+    }
+
+    public function updateUserPermissions($attributes = [])
+    {
+        $response = $this->post('updateuserpermissions.aspx', $attributes);
+
+        if ($response && $response->getStatusCode() == 200) {
+            return $this->getData($response);
+        }
+        return [];
+    }
+
+    private function get($path = '', $parameters = [])
+    {
+        $this->parameters = array_merge($this->parameters, $parameters);
+        $query = http_build_query($this->parameters);
+
+        try {
+            return $this->client->request('GET', $path.'?'.$query);
+        } catch (\Exception $e) {
+            \Log::error($e);
+            return false;
+        }
+    }
+
+    private function getData(Response $response)
+    {
+        return json_decode($response->getBody()->getContents());
+    }
+
+    private function post($path = '', $parameters = [])
+    {
+        $query = http_build_query($this->parameters);
+
+        try {
+            return $this->client->request('POST', $path.'?'.$query, [
+                'form_params' => $parameters
+            ]);
+        } catch (\Exception $e) {
+            \Log::error($e);
+            return false;
+        }
+    }
+}
---
+++ tests/AnswerbaseClientTest.php
@@ -0,0 +1,324 @@
+<?php
+
+use Mockery as m;
+use App\Clients\AnswerbaseClient;
+
+class AnswerbaseClientTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->client = new AnswerbaseClient();
+
+        $config = [
+            'api_key' => uniqid(),
+            'base_uri' => 'http://example.answerbase.com/api/',
+            'timeout' => '10.0',
+        ];
+
+        $this->client->client = m::mock('GuzzleHttp\Client');
+    }
+
+    public function testItCanCreateUser()
+    {
+        $attributes = [
+            'useremail' => uniqid(),
+        ];
+
+        $response = m::mock('GuzzleHttp\Psr7\Response');
+
+        $query = http_build_query($this->client->parameters);
+
+        $this->client->client->shouldReceive('request')
+            ->with('POST', 'registeruser.aspx?'.$query, [
+                'form_params' => $attributes
+            ])
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn('200');
+        $response->shouldReceive('getBody')
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getContents')
+            ->once()
+            ->andReturn($this->generateJsonUserResponse());
+
+        $result = $this->client->createUser($attributes);
+
+        $this->assertNotNull($result->user);
+    }
+
+    public function testItCanUpdateUserPermissions()
+    {
+        $attributes = [
+            'useremail' => uniqid(),
+        ];
+
+        $response = m::mock('GuzzleHttp\Psr7\Response');
+
+        $query = http_build_query($this->client->parameters);
+
+        $this->client->client->shouldReceive('request')
+            ->with('POST', 'updateuserpermissions.aspx?'.$query, [
+                'form_params' => $attributes
+            ])
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn('200');
+        $response->shouldReceive('getBody')
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getContents')
+            ->once()
+            ->andReturn(json_encode(true));
+
+        $result = $this->client->updateUserPermissions($attributes);
+
+        $this->assertTrue($result);
+    }
+
+    public function testItCanGetUserByEmailWhenUserExists()
+    {
+        $email = uniqid();
+
+        $requestString = 'getuser.aspx?'.$this->buildRequestString(['useremail' => $email]);
+
+        $response = m::mock('GuzzleHttp\Psr7\Response');
+
+        $jsonResponse = $this->generateJsonUserResponse();
+
+        $this->client->client->shouldReceive('request')
+            ->with('GET', $requestString)
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn('200');
+        $response->shouldReceive('getBody')
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getContents')
+            ->once()
+            ->andReturn($jsonResponse);
+
+        $result = $this->client->getUserByEmail($email);
+
+        $this->assertNotNull($result->user);
+    }
+
+    public function testItCanNotGetUserByEmailWhenUserNotExists()
+    {
+        $email = uniqid();
+
+        $requestString = 'getuser.aspx?'.$this->buildRequestString(['useremail' => $email]);
+
+        $response = m::mock('GuzzleHttp\Psr7\Response');
+
+        $jsonResponse = $this->generateJsonUserResponse();
+
+        $this->client->client->shouldReceive('request')
+            ->with('GET', $requestString)
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn('400');
+
+        $result = $this->client->getUserByEmail($email);
+
+        $this->assertEmpty($result);
+    }
+
+    public function testItCanNotGetUserByEmailWhenExceptionThrown()
+    {
+        $email = uniqid();
+
+        $requestString = 'getuser.aspx?'.$this->buildRequestString(['useremail' => $email]);
+
+        $response = m::mock('GuzzleHttp\Psr7\Response');
+
+        $jsonResponse = $this->generateJsonUserResponse();
+
+        $this->client->client->shouldReceive('request')
+            ->with('GET', $requestString)
+            ->once()
+            ->andThrow('\Exception');
+
+        $result = $this->client->getUserByEmail($email);
+
+        $this->assertEmpty($result);
+    }
+
+    public function testItCanGetCategoriesWhenExist()
+    {
+        $defaults = [
+            'maxresults' => '0',
+            'restrictToFirstLevel' => 'true',
+        ];
+        $requestString = 'getcategories.aspx?'.$this->buildRequestString($defaults);
+
+        $response = m::mock('GuzzleHttp\Psr7\Response');
+
+        $jsonResponse = $this->generateJsonCategoriesResponse();
+
+        $this->client->client->shouldReceive('request')
+            ->with('GET', $requestString)
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn('200');
+        $response->shouldReceive('getBody')
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getContents')
+            ->once()
+            ->andReturn($jsonResponse);
+
+        $result = $this->client->getCategories();
+
+        $this->assertNotNull($result->categories);
+    }
+
+    public function testItCanNotGetCategoriesWhenNotExists()
+    {
+        $defaults = [
+            'maxresults' => '0',
+            'restrictToFirstLevel' => 'true',
+        ];
+        $requestString = 'getcategories.aspx?'.$this->buildRequestString($defaults);
+
+        $response = m::mock('GuzzleHttp\Psr7\Response');
+
+        $jsonResponse = $this->generateJsonCategoriesResponse();
+
+        $this->client->client->shouldReceive('request')
+            ->with('GET', $requestString)
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn('400');
+
+        $result = $this->client->getCategories();
+
+        $this->assertEmpty($result);
+    }
+
+    public function testItCanGetQuestionsByCategoryIdWhenExist()
+    {
+        $category_id = uniqid();
+        $defaults = [
+            'categoryIds' => $category_id,
+            'orderby' => 'newest',
+            'fullquestiondetails' => 'true',
+            'maxresults' => '0',
+        ];
+        $requestString = 'getquestionslist.aspx?'.$this->buildRequestString($defaults);
+
+        $response = m::mock('GuzzleHttp\Psr7\Response');
+
+        $jsonResponse = $this->generateJsonQuestionsResponse();
+
+        $this->client->client->shouldReceive('request')
+            ->with('GET', $requestString)
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn('200');
+        $response->shouldReceive('getBody')
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getContents')
+            ->once()
+            ->andReturn($jsonResponse);
+
+        $result = $this->client->getQuestionsByCategoryId($category_id);
+
+        $this->assertNotNull($result->searchResult);
+    }
+
+    public function testItCanNotGetQuestionsByCategoryIdWhenNotExist()
+    {
+        $category_id = uniqid();
+        $defaults = [
+            'categoryIds' => $category_id,
+            'orderby' => 'newest',
+            'fullquestiondetails' => 'true',
+            'maxresults' => '0',
+        ];
+        $requestString = 'getquestionslist.aspx?'.$this->buildRequestString($defaults);
+
+        $response = m::mock('GuzzleHttp\Psr7\Response');
+
+        $jsonResponse = $this->generateJsonQuestionsResponse();
+
+        $this->client->client->shouldReceive('request')
+            ->with('GET', $requestString)
+            ->once()
+            ->andReturn($response);
+        $response->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn('400');
+
+        $result = $this->client->getQuestionsByCategoryId($category_id);
+
+        $this->assertEmpty($result);
+    }
+
+    private function buildRequestString($params = [])
+    {
+        return http_build_query(
+            array_merge($this->client->parameters, $params)
+        );
+    }
+
+    private function generateJsonCategoriesResponse($params = [])
+    {
+        return '{
+            "categories": {
+                "@name": "A History of Modern Computing (Halsted)",
+                "@id": "298974"
+            }
+        }';
+    }
+
+    private function generateJsonQuestionsResponse($params = [])
+    {
+        return '{
+            "searchResult": {
+                "tags": null,
+                "questions": null
+            }
+        }';
+    }
+
+    private function generateJsonUserResponse($params = [])
+    {
+        return '
+        {
+          "user": {
+            "@username": "KarlHughes",
+            "@id": "1588842",
+            "@sso_userid": "",
+            "@firstname": "Karl",
+            "@lastname": "Hughes",
+            "@title": "Head of Engineering, Packback",
+            "@organization": "",
+            "@website": "",
+            "@twitterUsername": "",
+            "@address": "",
+            "@city": "",
+            "@region": "",
+            "@country": "",
+            "@profileEmail": "karl@packback.co"
+          }
+        }';
+    }
+}
